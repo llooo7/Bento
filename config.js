a10c3eaea03134239b86c02e3322c4009c263d7e
llooo7 <32995823+llooo7@users.noreply.github.com>
2021-11-29 23:03:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ const CONFIG = {
   greetingNight: 'Go to Sleep',
 
   // Weather
-  weatherKey: 'InsertYourAPIKeyHere123456',
+  weatherKey: '6e97e784135bf28f4aacfa9cae2444af',
   weatherIcons: 'OneDark', // 'Nord', 'Dark', 'White'
   weatherUnit: 'C', // 'F', 'C'
   language: 'en', // More languages in https://openweathermap.org/current#multi
